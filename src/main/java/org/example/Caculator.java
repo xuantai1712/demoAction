1e45ec19f3cc30799e11e1d1158ea8a1a3aed619
tai <taibvps37458@fpt.edu.vn>
2024-11-23 20:01:48 +0700

    first commit

---
+++ src/main/java/org/example/Caculator.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Caculator implements ICaculator{
+
+    @Override
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+
+    @Override
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+}
