44b0b949c3cba393faadcf3f85546a9c1571aa0e
tai <taibvps37458@fpt.edu.vn>
2024-11-23 21:35:52 +0700

    test pass

--- src/main/java/org/example/Caculator.java
+++ src/main/java/org/example/Caculator.java
@@ -4,11 +4,11 @@ public class Caculator implements ICaculator{
 
     @Override
     public Integer sub(Integer a, Integer b) {
-        return a +b;
+        return a + b;
     }
 
     @Override
     public Integer sum(Integer a, Integer b) {
-        return a-b ;
+        return a- b ;
     }
 }
